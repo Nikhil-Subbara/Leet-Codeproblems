64ce329a2f2e00a35b745928ab78e4e1ac2c22fd
Nikhil Subbara <subbaranikhil29@gmail.com>
2026-08-20 10:39:08 +0530

    Enhance isAnagram function with comments
    Added comments to explain the logic for counting letters in strings and checking for anagrams.

--- 0242-valid-anagram/0242-valid-anagram.c
+++ 0242-valid-anagram/0242-valid-anagram.c
@@ -3,13 +3,14 @@ bool isAnagram(char* s, char* t)
 {
     if(strlen(s)!=strlen(t))
     return false;
+    // For 26 letters
     int count[26] = {0};
-
+    // counting letters in s like how many a's 
     for (int i = 0; s[i] != '\0'; i++)
     {
         count[s[i] - 'a']++;
     }
-
+//checking the Same no. of a's in t
     for (int i = 0; t[i] != '\0'; i++)
     {
         count[t[i] - 'a']--;
@@ -17,6 +18,7 @@ bool isAnagram(char* s, char* t)
 
     for (int i = 0; i < 26; i++)
     {
+        //if there are same no.of letters then there no entry in if
         if (count[i] != 0)
             return false;
     }
