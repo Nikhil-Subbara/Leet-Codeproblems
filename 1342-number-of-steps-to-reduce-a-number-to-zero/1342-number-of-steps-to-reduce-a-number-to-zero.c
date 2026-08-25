335a77c19450c5c6196cfe923fc0d464176978e1
Nikhil Subbara <subbaranikhil29@gmail.com>
2026-08-25 19:19:17 +0530

    Time: 0 ms (100%), Space: 8.6 MB (68.51%) - LeetHub

---
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.c
@@ -0,0 +1,18 @@
+int numberOfSteps(int num) 
+{
+   int step=0;
+   int rem=0;
+   while(num!=0)
+   {
+    if(num%2==0)
+      {
+        num=num/2;
+      }
+      else
+      {
+        num=num-1;
+      }
+      step++;
+   } 
+   return step;
+}
